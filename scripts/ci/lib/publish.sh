43098074ac44ade2ec4057834ced3a4066d2caa1
Yousaf Nabi <yousafn@gmail.com>
2022-04-19 23:46:32 +0100

    ci: dry run

--- scripts/ci/lib/publish.sh
+++ scripts/ci/lib/publish.sh
@@ -15,5 +15,5 @@ echo "--> Releasing version ${VERSION}"
 
 echo "--> Releasing artifacts"
 echo "    Publishing cypress-multi-reporters@${VERSION}..."
-npm publish --tag latest
+npm publish --tag latest --dry-run
 echo "    done!"
