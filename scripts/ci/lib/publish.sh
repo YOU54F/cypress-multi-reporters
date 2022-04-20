f2740ca38bc7007969e490109dcc164736e4df5d
Yousaf Nabi <yousafn@gmail.com>
2022-04-20 02:09:57 +0100

    chore: release v1.6.1

--- scripts/ci/lib/publish.sh
+++ scripts/ci/lib/publish.sh
@@ -15,5 +15,5 @@ echo "--> Releasing version ${VERSION}"
 
 echo "--> Releasing artifacts"
 echo "    Publishing cypress-multi-reporters@${VERSION}..."
-npm publish --tag latest --dry-run
+npm publish --tag latest
 echo "    done!"
