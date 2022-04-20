a536a323bb9394b7e7a173d14ab4f0fcf5010c06
Yousaf Nabi <yousafn@gmail.com>
2022-04-20 02:22:00 +0100

    fix: maybe order is important idk

--- scripts/ci/lib/publish.sh
+++ scripts/ci/lib/publish.sh
@@ -15,5 +15,5 @@ echo "--> Releasing version ${VERSION}"
 
 echo "--> Releasing artifacts"
 echo "    Publishing cypress-multi-reporters@${VERSION}..."
-npm publish --tag latest --access public
+npm publish --access public --tag latest
 echo "    done!"
